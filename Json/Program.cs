36c5bef0d1ee30c28875a355e4b81dd7b7c48c42
Daniel <dscam992@learner.dvusd.org>
2022-03-22 09:30:51 -0700

    Fixed Class

--- Json/Program.cs
+++ Json/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
+using System.Text.Json;
+using static System.Console;
 
 namespace Json
 {
-   
+    public class Program
+    {
+
+        public class WeatherForecast
+        {
+            public DateTimeOffset Date { get; set; }
+            public int TemperatureCelsius { get; set; }
+            public string Summary { get; set; }
+        }
+        public static void Main()
+        {
+            var weatherForecast = new WeatherForecast
+            {
+                Date = DateTime.Parse("2019-08-01"),
+                TemperatureCelsius = 25,
+                Summary = "Hot"
+            };
+            var phoenixForecast = new WeatherForecast
+            {
+                Date = DateTime.Parse("2022-03-22"),
+                TemperatureCelsius = 28,
+                Summary = "Warm"
+            };
+
+            string jsonString = JsonSerializer.Serialize(weatherForecast);
+            WriteLine(jsonString);
+
+            string phoenixString = JsonSerializer.Serialize(phoenixForecast);
+            WriteLine(phoenixString);
+
+            WeatherForecast w = JsonSerializer.Deserialize<WeatherForecast>(phoenixString);
+            WriteLine(w.Summary);
+            ReadKey();
+        }
+    }
 }
 
